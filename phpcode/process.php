2a8e97b2c00e7d2a8e46f4f1e934ce1f4435a228
Bijoy Ahamed <bijoyahmed818@gmail.com>
2025-05-16 22:47:38 +0600

    Validation with data display

---
+++ phpcode/process.php
@@ -0,0 +1,10 @@
+<?php
+echo " HEllo <br>";
+echo "Hi:  ".$_POST['fName'];
+echo "<br>Email: ".$_POST['Email'];
+echo "<br>Date of Birth: ".$_POST['birthday'];
+echo "<br>Country: ".$_POST['Country'];
+echo "<br>Gender:  ".$_POST['gender'];
+echo "<br>Favourite Color:  ".$_POST['color'];
+echo "<br>Opinion:  ".$_POST['Opinion'];
+?>
